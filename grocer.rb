a5a2714d5ab6ec5f4b376878e6efcc3a23eb0508
Robert Tate <btate712@gmail.com>
2019-03-05 02:32:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,40 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+	return_hash = {}
+  cart.each do |item|
+		item.each do |key, values|
+      return_hash[key] = {}
+      return_hash[key][:price] = values[:price]
+      return_hash[key][:clearance] = values[:clearance]
+      return_hash[key][:count] = cart.count(item)
+    end
+	end
+  return_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  #fix me
+  coupons.each do |coupon|
+    new_item_name = ""
+    new_item_hash = {}
+    cart.each do |item, info|
+      if item == coupon[:item] && info[:count] >= coupon[:num]
+        new_item_name = "#{coupon[:item]} W/COUPON"
+        info[:count] -= coupon[:num]
+        new_item_hash[:price] = coupon[:cost]
+        new_item_hash[:clearance] = info[:clearance]
+        new_item_hash[:count] = 1
+      end
+    end
+    cart[new_item_name] = new_item_hash
+  end
+binding.pry
+	cart = consolidate_cart(cart)
+
+	cart
 end
+
 
 def apply_clearance(cart)
   # code here
